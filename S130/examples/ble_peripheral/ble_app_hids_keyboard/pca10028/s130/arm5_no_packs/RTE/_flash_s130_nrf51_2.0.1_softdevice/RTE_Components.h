85c01fd61477e8bbbe925d3799c1ce861fa0e07e
kbladewht <w00110601111@163.com>
2025-01-15 08:38:54 +0800

    add sdk130

---
+++ S130/examples/ble_peripheral/ble_app_hids_keyboard/pca10028/s130/arm5_no_packs/RTE/_flash_s130_nrf51_2.0.1_softdevice/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_hids_keyboard_pca10028_s130' 
+ * Target:  'flash_s130_nrf51_2.0.1_softdevice' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
